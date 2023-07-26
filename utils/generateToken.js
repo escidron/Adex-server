3d35b3932865b03fb6b6d4971cd828bc46203001
escidron <eduardosanchezcidron@gmail.com>
2023-07-26 02:21:27 -0300

    debuging token

--- utils/generateToken.js
+++ utils/generateToken.js
@@ -4,10 +4,10 @@ const generateToken = (res, userId,fullName,email) => {
   const token = jwt.sign({ userId,fullName,email }, 'usersecrettoken', {
     expiresIn: '30d',
   });
-  console.log('generating token')
+  console.log('generating token',userId)
   res.cookie('jwt', token, {
     httpOnly: true,
-    secure: true, // Use secure cookies in production
+    secure: false, // Use secure cookies in production
     // secure: process.env.NODE_ENV !== 'development', // Use secure cookies in production
     sameSite: 'strict', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
