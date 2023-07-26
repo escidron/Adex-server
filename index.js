a114c306df2a820e1ae2167d106bff0d4a8e8368
escidron <eduardosanchezcidron@gmail.com>
2023-07-25 23:44:47 -0300

    change the client ip again

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 5000;
 
 const io = new Server({
   cors: {
-    origin: 'https://adex-client.vercel.app',
+    origin: 'https://adex-sable.vercel.app/',
   },
 });
 
@@ -146,7 +146,7 @@ io.listen(4000);
 const app = express();
 app.use(express.json({ limit: "100mb" }));
 const corsOptions = {
-  origin: 'https://adex-client.vercel.app',
+  origin: 'https://adex-sable.vercel.app/',
   credentials: true,
   exposedHeaders: ["Authorization"],
   allowedHeaders: [
