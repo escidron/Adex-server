f12e5d1b9859d116ceb88b75041cc0f7f1497caa
escidron <eduardosanchezcidron@gmail.com>
2023-08-10 23:12:52 -0300

    fix user name does appearing in the navbar

--- controllers/userController.js
+++ controllers/userController.js
@@ -376,7 +376,9 @@ const getExternalAccount = asyncHandler(async (req, res) => {
         if (err) throw err;
         if (result.length == 0) {
           res.status(200).json({
-            data: "",
+            name: firstName,
+            image: image,
+            userId: userId,
           });
         } else {
           const externalAccount = result[0].external_account_id;
