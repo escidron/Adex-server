38f78aaa02f50bc13708269723d662d38f3015bb
escidron <eduardosanchezcidron@gmail.com>
2023-10-15 20:02:37 -0300

    testint email template

--- utils/emailTamplates/emailTemplate.js
+++ utils/emailTamplates/emailTemplate.js
@@ -11,10 +11,7 @@ export default function renderEmail(data) {
       <body style="margin: 0;padding: 0;font-family: Optimistic Text, Helvetica, sans-serif">
         <div style="background-color: #e5e5e3;max-width: 600px;margin: 20px auto;padding: 20px;align-items: center;border-radius: 10px;font-family: Optimistic Text, Helvetica, sans-serif">
           <div class="header" style="text-align: center;margin-top: 20px">
-            <img
-              src="http://test.adexconnect.com/email/email-logo.png"
-              alt="Company Logo"
-            />
+
           </div>
           <h1 class="title" style="color:black;font-size:32px;margin: 20px 0 0 0;text-align:center">{{ title }}</h1>
           <h1 class="sub-title" style="color:black;font-size:32px;margin: 0;font-weight: 500;text-align:center;">{{ subTitle }}</h1>
@@ -78,12 +75,7 @@ export default function renderEmail(data) {
                     margin-right: auto;">
             ADEX Login
           </a>
-          <img
-            src="http://test.adexconnect.com/email/{{ icon }}.png"
-            alt="{{ icon }}"
-            class="message-image"
-            style="margin-top: 40px;display: block;margin-left: auto;margin-right: auto;"
-          />
+
           <h1 class="footer-text" style="color:black;width: 80%;font-size: 14px;text-align: center;margin-top: 50px;display: block;margin-left: auto;margin-right: auto;">
             Thank you for choosing ADEX for your advertising needs. If you are having issues with your booking, please contact us.
           </h1>
@@ -92,13 +84,7 @@ export default function renderEmail(data) {
         <div class="social-icons" style="text-align: center;margin-top: 20px;">
           <h5 style="color:black;text-decoration: none;margin: 0 10px;">info@adexconnect.com</h5>
           <div class="icons-group" style="margin-left: auto;margin-right: auto;width: 100px;margin-top: 10px;">
-            <a href="https://facebook.com"
-              ><img
-                src="http://test.adexconnect.com/email/facebook-logo.png"
-                alt="Facebook"
-                class="social-media-icons"
-                style="height: 16px;"
-            /></a>
+
             <a href="https://instagram.com"
               ><img
                 src="http://test.adexconnect.com/email/instagram-logo.png"
@@ -106,13 +92,7 @@ export default function renderEmail(data) {
                 class="social-media-icons"
                 style="height: 16px;"
             /></a>
-            <a href="https://youtube.com"
-              ><img
-                src="http://test.adexconnect.com/email/youtube-logo.jpg"
-                alt="Instagram"
-                class="social-media-icons"
-                style="height: 16px;"
-            /></a>
+
           </div>
         </div>
       </body>
