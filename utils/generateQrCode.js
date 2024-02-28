416e5f6cb9fcee4704917c66cbab1d2ed5889a4c
escidron <eduardosanchezcidron@gmail.com>
2024-02-27 21:57:24 -0300

    change the qr code layout

--- utils/generateQrCode.js
+++ utils/generateQrCode.js
@@ -7,12 +7,12 @@ export async function generateQrCode(listingId) {
     const qrCodeContent = `https://adexconnect.com/market-place/details?id=${listingId}`;
     const qrCodeImage = await qrcode.toDataURL(qrCodeContent);
 
-  const imagePath = new URL('../images/email/adex-logo-white-yellow.png', import.meta.url);
+  const imagePath = new URL('../images/email/adex-logo-black-yellow.png', import.meta.url);
     const imageBuffer = await fs.readFile(imagePath);
     const base64Image = imageBuffer.toString('base64');
 
     const htmlContent = `
-      <div style="background-color: black; color: white; text-align: center; padding: 50px;">
+      <div style="background-color: white; color: white;border: 1px solid black; text-align: center; padding: 50px;">
           <img src="data:image/jpeg;base64,${base64Image}" alt="Foto do Listing" style="width: 70px; height: 70px;" />
           <p style="margin-top: 10px; font-family: 'Inter', sans-serif;">Reach more people, place your ad here.</p>
           <img src="${qrCodeImage}" alt="QR Code" style="border-radius: 10px; margin-top: 20px; width: 180px; height: 180px;" />
