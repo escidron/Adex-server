8c11b51230a35b6ff192d7c45453ba9781c6a418
escidron <eduardosanchezcidron@gmail.com>
2025-02-18 22:33:11 -0300

    add missing fields in notifications table

--- models/notifications.model.js
+++ models/notifications.model.js
@@ -1,10 +1,13 @@
 const createNotificationsTable = `
 CREATE TABLE IF NOT EXISTS notifications (
     id bigint UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    business_type varchar(255),
     created_at timestamp DEFAULT CURRENT_TIMESTAMP,
-    updated_at timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at timestamp
+    user_id varchar(45),
+    header varchar(255),
+    message varchar(255),
+    redirect varchar(255),
+    read varchar(1)
+    key varchar(45)
   );
 `;
 
